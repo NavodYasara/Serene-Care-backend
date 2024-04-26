8880feec421d0f8a2144c4398463f7a41b30d03d
NavodYasara <111131809+NavodYasara@users.noreply.github.com>
2024-04-26 21:12:29 +0530

    26/04 - Refactor login and registration functionality in userController.js and userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {login, registerCaretaker} from '../Controller/usercontroller.js';
+import {login, registerCaretaker, userDetails} from '../Controller/usercontroller.js';
 
 const router = express.Router();
 
 router.post('/userRoutes', registerCaretaker); // Define the route for the login controller
 router.post('/login',login); // Define the route for the register controller
+router.get('/user', userDetails); // Define the route for the userDetails controller
 
 
 export default router;
