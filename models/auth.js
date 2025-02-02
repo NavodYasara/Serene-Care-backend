9287796fc84a057a119283e5f8654cef85f54f27
Navod Yasara <navodyasara567@gmail.com>
2025-02-02 11:34:45 +0530

    refactor: implement register service in authService and remove auth model

--- models/auth.js
+++
@@ -1,3 +0,0 @@
-import db from '../config/db.js' ;
-import jwt from 'jsonwebtoken' ;
-import bcrypt from 'bcrypt' ;
---
+++ models/authService.js
@@ -0,0 +1,19 @@
+import db from '../config/db.js' ;
+import jwt from 'jsonwebtoken' ;
+import bcrypt from 'bcrypt' ;
+import { promise } from 'bcrypt/promises.js';
+
+
+export default registerService = () => {
+    return new promise((resolve,reject) => {
+        const userRegister = `INSERT INTO users (firstName,lastName,userName,email,password,role) values (?,?,?,?,?,?)`;
+        db.query(userRegister, [userName,email,password], (err, result) => {
+            if(err) {
+                reject(err);
+            }
+            resolve(result);
+        });
+    });
+};
+
+// export default loginService = () => {};
