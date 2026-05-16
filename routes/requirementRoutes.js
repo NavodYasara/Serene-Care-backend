2d5bb976082532c43b0a3c9f76bd814cc85cd90e
Navod Yasara <navodyasara567@gmail.com>
2026-05-16 23:10:20 +0530

    fix: remove unnessesary function and import "insertRequirement"

--- routes/requirementRoutes.js
+++ routes/requirementRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
-import {  insertRequest,insertRequirement, getAllcaretakers } from '../Controller/requirementController.js';
+import {  insertRequest, getAllcaretakers } from '../Controller/requirementController.js';
 
 const router = express.Router();
 
-router.post('/insertRequirement', insertRequirement); // Route to insert a new requirement
 router.post('/insertRequest', insertRequest); // Route to insert a new request
 router.get('/getAllcaretakers', getAllcaretakers); // Route to get all caretakers
 // router.get("/getAllRequirements", getAllRequirements); // Route to get all requirements
