7e43620b8ab437b7692c77c3137a915aa21e0c93
Navod Yasara <navodyasara567@gmail.com>
2026-05-16 22:06:40 +0530

    fix: requirementController to handle caretaker requirement insertion and data retrieval

--- Controller/requirementController.js
+++ Controller/requirementController.js
@@ -2,42 +2,15 @@ import { db } from "../db.js";
 
 //##############  Controller function to insert details into the requirement table of the database ###############################
 
-export const insertRequirement = (req, res) => {
-  const {
-    requirement,
-    startDate,
-    endDate,
-    preffGender,
-    status,
-    userId,
-    caretakerId,
-  } = req.body;
-
-  db.query(
-    "INSERT INTO requirement (requirement, startDate, endDate, preffGender, status, userId, caretakerId) VALUES (?, ?, ?, ?, ?, ?, ?)",
-    [requirement, startDate, endDate, preffGender, status, userId, caretakerId],
-    (err, results) => {
-      if (err) {
-        console.error("Error during requirement insertion:", err);
-        return res
-          .status(500)
-          .json({ error: "Internal Server Error", details: err.message });
-      }
-
-      res.status(201).json({ message: "Requirement inserted successfully" });
-    },
-  );
-};
-
 export const insertRequest = (req, res) => {
   console.log(req.body);
-  const { startDate, endDate, requirement, userId } = req.body;
+  const { startDate, endDate, requirement, userId, preffGender } = req.body;
 
   if (!userId) {
     return res.status(400).json({ error: "User ID is required" });
   }
 
-  // First, get the caretakerId from the caretaker table using userId
+  // Get the caretakerId from the caretaker table using userId
   db.query(
     "SELECT caretakerId FROM caretaker WHERE userId = ?",
     [userId],
